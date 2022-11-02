a19f39be712e8d6c0b4f766f0874133a455f4ca4
kashyapbharadwaj <115248837+kashyapbharadwaj@users.noreply.github.com>
2022-11-02 20:23:44 +0530

    Add files via upload

---
+++ positive numbers in a range.py
@@ -0,0 +1,18 @@
+list1=[12,-7,5,64,14]
+list2=[12,14,-95,3]
+i=0
+m=len(list1)
+while i<m:
+    if list1[i]>0:
+        print([list1[i]])
+    i+=1
+j=0
+n=len(list2)
+while j<n:
+    if list2[j]>0:
+        print([list2[j]])
+    j+=1
+
+
+
+
